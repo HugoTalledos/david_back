15d1e52664d7b44b7727ecc42ef91536a8d0ebbb
HugoTalledos <hugo.talledos@bpsmart.ai>
2024-01-25 21:19:09 -0500

    Add set repository

--- repository/SetRepository.js
+++ repository/SetRepository.js
@@ -1,106 +1,137 @@
-const axios = require('axios');
+const { firestoreRef } = require('../drivers/firestore');
+const songRepository = require('./SongRepository');
 const logger = require('../utils/logger');
-const { API_GRAPQL } = require('../utils/config');
 const log = logger({ fileName: 'SetRepository.js' });
-const url = API_GRAPQL;
 
-const getSongsById = (songList) => {
-  let parameters = '';
-  songList.forEach((element, idx) => {
-    let querybase = `
-    song${idx}: song(id: "${element}") {
-      data {
-        id
-        attributes {
-          so_name,
-          so_bpm,
-          so_tonality,
-          so_resource,
-          multitrack {
-            data {
-              id,
-              attributes {
-                url,
-              }
-            }
-          },
-          lyrics {
-            data {
-              attributes {
-                ly_lyric
-                ly_chords
-              }
-            }
-          },
-          singers {
-            data {
-              attributes {
-                si_name
-              }
-            }
-          }
-        }
-      }
-    }`;
-    parameters += querybase;
-  });
-  const query = `{
-    ${parameters}
-  }`;
+const collection = 'public';
 
-  return axios.post(url, { query })
-    .then(({ data }) => {
-      log.info('Success query');
-      const [songsObjects] = Object.values(data);
-      const songsList = Object.values(songsObjects);
-      return songsList;
-    })
-    .catch((error) => {
-      log.error(error.message);
-      return null;
+const createSet = async (setId, body) => {
+  try {
+    const setDoc = await firestoreRef.collection(collection)
+      .doc(setId);
+
+    await setDoc.set({
+      setId,
+      status: true,
+      ...body,
+      createdAt:(new Date()).toGMTString(),
+      updatedAt:(new Date()).toGMTString(),
     });
-    
+
+    log.info(`Set created successfully`);
+    return setId;
+  } catch (e) {
+    log.error(e);
+    return null;
+  }
 };
 
-const getAllSongs = () => {
-  let querybase = `
-    songs {
-      data {
-        id
-        attributes {
-          so_name
-          so_bpm
-          so_tonality
-          so_resource
-          singers {
-            data {
-              id
-              attributes {
-                si_name
-              }
-            }
-          }
-        }
-      }
-    }`;
-  const query = `{${querybase}}`;
+const updateSet = async (setId, data) => {
+  try {
+    const route = await firestoreRef.collection(collection)
+      .doc(setId);
 
-  return axios.post(url, { query })
-    .then(({ data }) => {
-      log.info('Success query');
-      const [songsObjects] = Object.values(data);
-      const songsList = Object.values(songsObjects);
-      return songsList;
-    })
-    .catch((error) => {
-      log.error(error.message);
+    const doc = await route.get();
+    if (!doc.exists) {
+      log.warn(`${setId} no existe o eliminado`);
       return null;
-    });
-    
+    }
+
+    const body = {
+      setId,
+      ...data,
+      updatedAt:( new Date()).toGMTString(),
+    }; 
+    await route.set(body, { merge: true });
+
+    log.info(`Set updated successfully`);
+    return body;
+  } catch (e) {
+    log.error(e);
+    return null;
+  }
 };
 
+
+const getSetById = async (setId) => {
+  try {
+    const route = await firestoreRef.collection(collection)
+      .doc(setId);
+    const doc = await route.get();
+    const set = doc.data();
+    const { status: setStatus, songList } = set;
+
+    if (!doc.exists || !setStatus) {
+      log.warn('Set not found or deleted');
+      return null;
+    }
+    
+    log.info(`Canciones encontradas dentro del set: ${songList.length}`);
+    let songs = [];
+
+    for (let i = 0; i < songList.length; i++) {
+      const id = songList[i];
+      const song = await songRepository.getSongById(id);
+      if (song) songs.push(song);
+    }
+
+    return { ...set, songConfig: songs };
+  } catch (e) {
+    log.error(e);
+    return null;
+  }
+};
+
+const getAllSets = async () => {
+  try {
+    const route = await firestoreRef.collection(collection).where("status", "==", true).get();
+
+    if (route.size <= 0) return [];
+
+    log.info(`Sets found: ${route.size}`);
+    const setList = [];
+    for(let i = 0; i < route.size; i++) {
+      const setId = route.docs[i].id;
+      const set = await getSetById(setId);
+      setList.push(set);
+    }
+
+    if (setList.length <= 0) return [];
+
+    return setList;
+  } catch(e) {
+    log.error(e);
+    return null;
+  }
+};
+
+const deleteSetFromDb = async (setId) => {
+  try {
+    const doc = await firestoreRef.collection(collection)
+      .doc(setId);
+    
+    const values = await doc.get();
+    if (!values.exists) {
+      log.error(`Set ${setId} doesn't exist`);
+      return null;
+    }
+
+    await doc.set({
+      status: false,
+      updatedAt:  (new Date()).toGMTString(),
+    }, { merge: true });
+    log.info(`Set ${setId} deleted`);
+    return true;
+  } catch (e) {
+    log.error(e);
+    return null;
+  }
+};
 
 module.exports = {
-  getAllSongs,
-  getSongsById
+  createSet,
+  updateSet,
+  getSetById,
+  getAllSets,
+  deleteSetFromDb,
 };
--- service/Set.js
+++ service/Set.js
@@ -1,56 +1,17 @@
 const logger = require('../utils/logger');
-
-const { firestoreRef } = require('../drivers/firestore');
 const { generateId, successResponse, errorToResponse } = require('../utils/utils');
 const setRepository = require('../repository/SetRepository');
 
 const log = logger({ fileName: 'Set.js' });
-const collection = 'public';
-const songCollection = 'songs';
 
 const createSetInDb = async (req, res) => {
-  const { setName, setDescription, songList } = req.body;
-  const registerBy = 'test';
+  const { setName, ...all } = req.body;
   const setId = generateId();
   log.info(`Creating Set ${setName}`);
     
   try {
-    if (!firestoreRef) {
-      log.error('Couldn\'t connect to database');
-      return res.status(500).send(errorToResponse('Couldn\'t connect to database'));
-    }
-    const setDoc = await firestoreRef.collection(collection)
-      .doc(setId);
-
-    await setDoc.set({
-      setId,
-      setName,
-      setDescription,
-      status: true,
-      createdAt:(new Date()).toGMTString(),
-      updatedAt:(new Date()).toGMTString(),
-      registerBy,
-      updatedBy: registerBy
-    });
-    log.info(`Set created successfully`);
-
-    const songConfigDoc = setDoc.collection(songCollection);
-
-    for (let i = 0; i < songList.length; i++) {
-      const { songId, ...all } = songList[i];
-      const songDoc = songConfigDoc.doc(songId);
-  
-      await songDoc.set({
-        songId,
-        ...all,
-        status: true,
-        createdAt: (new Date()).toGMTString(),
-        updatedAt: (new Date()).toGMTString(),
-        registerBy,
-        updatedBy: registerBy
-      });
-    }
-    log.info(`Config song created successfully`);
+    const createdSet = await setRepository.createSet(setId, { setName, ...all });
+    if (!createdSet) return res.status(500).send(errorToResponse(err, 'Error creating data in firestore'));
     return res.send(successResponse({ setId }))
   } catch (err) {
     log.error(err);
@@ -59,82 +20,29 @@ const createSetInDb = async (req, res) => {
 };
 
 const updateSetInDb = async (req, res) => {
-  const { setId, songList = [], ...all } = req.body;
+  const { setId, ...all } = req.body;
   log.info(`Updating set ${setId}`);
-  const updatedBy = 'updated';
 
   try {
-    if (!firestoreRef) {
-      log.error('Couldn\'t connect to database');
-      return res.status(500).send(errorToResponse('Couldn\'t connect to database'));
-    }
-    const route = await firestoreRef.collection(collection)
-      .doc(setId);
+    const updatedSet = await setRepository.updateSet(setId, all);
 
-    const doc = await route.get();
-    if (!doc.exists) return res.status(500).send(errorToResponse('Set not found or deleted'));
-
-    await route.set({
-      setId,
-      ...all,
-      updatedAt:( new Date()).toGMTString(),
-      updatedBy
-    }, { merge: true });
-    log.info(`Set updated successfully`);
-
-    const songConfigCollect = route.collection(songCollection); 
-
-    await songConfigCollect.get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => { doc.ref.update({ status: false }) })
-      });
-
-    for (let i = 0; i < songList.length; i++) {
-      const { songId, ...all } = songList[i];
-      const songConfig = songConfigCollect.doc(songId);
-      log.info(`Updating song config: ${songId}`);
-
-      await songConfig.set({
-        songId,
-        ...all,
-        status: true,
-        updatedAt: (new Date()).toGMTString(),
-        updatedBy
-      }, { merge: true });
-    }
-    log.info(`Config song updated successfully`);
-    return res.send(successResponse('Success updated'));
+    if (!updatedSet)  return res.status(500).send(errorToResponse(`Set ${setId} does not exist or deleted`));
+    return res.send(successResponse(updatedSet, 'Success updated'));
   } catch (error) {
     log.error(error);
-    return res.status(500)
-      .send(errorToResponse(`Unnexpected error`));
+    return res.status(500).send(errorToResponse(`Unnexpected error`));
   }
 };
 
 const getSetFromDb = async (req, res) => {
   const { setId } = req.params;
-  const { local = false } = req.body;
   log.info(`Getting set ${ setId }`);
   try {
-    if (!firestoreRef) {
-      log.error('Couldn\'t connect to database');
-      return res.status(500).send(errorToResponse('Couldn\'t connect to database'));
-    }
+    const setInfo = await setRepository.getSetById(setId);
 
-    const route = await firestoreRef.collection(collection)
-      .doc(setId);
-    const doc = await route.get();
-    const set = doc.data();
-    const { songIdList, status: setStatus } = set;
+    if (!setInfo) return res.status(409).send(errorToResponse('Set information not found or deleted'));
 
-    if (!doc.exists || !setStatus)
-      return local ? {} : res.status(409).send(errorToResponse('Set not found or deleted'));
-    
-    const filterSongs = await (route.collection(songCollection).where('status', '==', true)).get();
-    const songConfig = filterSongs.docs.map((doc) => doc.data());
-    const response = { ...set, songConfig };
-
-    return local ? response : res.send(successResponse(response));
+    return res.send(successResponse(setInfo));
   } catch (error) {
     log.error(error);
     return res.status(500).send(errorToResponse('Unnexpected error'));
@@ -145,25 +53,10 @@ const getAllSetsFromDb = async (req, res) => {
   log.info('Getting all sets');
 
   try {
-    if (!firestoreRef) {
-      log.error('Couldn\'t connect to database');
-      return res.status(500).send(errorToResponse('Couldn\'t connect to database'));
-    }
+    const setConfigList = await setRepository.getAllSets();
 
-    const route = await firestoreRef.collection(collection).where("status", "==", true).get();
-
-    if (route.size <= 0) return res.send(successResponse([], 'Sets not found'));
-    log.info(`Sets found: ${route.size}`);
-    const setList = [];
-    for(let i = 0; i < route.size; i++) {
-      const setId = route.docs[i].id;
-      const set = await getSetFromDb({ params: { setId }, body: { local: true } });
-      setList.push(set);
-    }
-
-    if (setList.length <= 0) return res.send(successResponse(setList, 'Results not found'));
-
-    return res.send(successResponse(setList, 'Success query'));
+    if (!setConfigList) return res.send(successResponse([], 'Sets not found'));
+    return res.send(successResponse(setConfigList, 'Success query'));
   } catch (error) {
     log.error(error.message);
     return res.status(500).send(errorToResponse('Unnexpected error'));
@@ -174,20 +67,9 @@ const deleteSetFromDb = async (req, res) => {
   const { setId } = req.params;
   log.info(`Deleting set ${setId}`);
   try {
-    const doc = await firestoreRef.collection(collection)
-      .doc(setId);
-    const values = await doc.get();
-    if (!values.exists) {
-      log.error(`Set ${setId} doesn't exist`);
-      return res.status(500).send(errorToResponse(`Product ${setId} does not exist`));
-    }
+    const deletedSet = await setRepository.deleteSetFromDb(setId);
 
-    await doc.set({
-      status: false,
-      updatedAt:  (new Date()).toGMTString(),
-      updatedBy: 'deleted'
-    }, { merge: true });
-    log.info(`Set ${setId} deleted`);
+    if (!deletedSet) return res.status(500).send(errorToResponse(`Product ${setId} does not exist`));
     return res.send(successResponse(`Set ${setId} delete succesfull`));
   } catch (error) {
     log.error(error.message);
